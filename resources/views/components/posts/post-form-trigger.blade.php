3fa22176c66d3f2f4f00ff25dca62c9da975f77a
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-10-29 14:42:06 +0100

    added posts.post-form-trigger component with styles fixes

---
+++ resources/views/components/posts/post-form-trigger.blade.php
@@ -0,0 +1,14 @@
+<div class="open_model" @click="$dispatch('open-form')">
+    <div class="open_ps">
+        <img src="{{ auth()->user()->image() }}" alt="user">
+        <input type="text" placeholder="What are you thinking about?" readonly>
+    </div>
+    <div class="cta_icons">
+        <div class="icons">
+            <i class='bx bx-camera'></i>
+            <i class='bx bx-image-alt'></i>
+            <i class='bx bx-happy'></i>
+        </div>
+        <button>Post</button>
+    </div>
+</div>
