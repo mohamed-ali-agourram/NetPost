21a61a345bc3b6e5e6ea1b2acdae8b96ad94e4ee
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-10-29 14:41:08 +0100

    added posts.post-form-trigger component with styles fixes

--- resources/views/components/posts/posts-list.blade.php
+++ resources/views/components/posts/posts-list.blade.php
@@ -1,20 +1,5 @@
 <div class="posts">
-    <div class="open_model" @click="$dispatch('open-form')">
-        <div class="open_ps">
-            <a href="./profie.html">
-                <img src="{{ auth()->user()->image() }}" alt="user">
-            </a>
-            <input type="text" placeholder="What are you thinking about?" readonly>
-        </div>
-        <div class="cta_icons">
-            <div class="icons">
-                <i class='bx bx-camera'></i>
-                <i class='bx bx-image-alt'></i>
-                <i class='bx bx-happy'></i>
-            </div>
-            <button id="post">Post</button>
-        </div>
-    </div>
+    <x-posts.post-form-trigger />
     @forelse ($this->posts as $post)
         <x-posts.post-card :key="'post-'.$post->id" :$post />
     @empty
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -39,20 +39,7 @@
 
         <div class="profile_body">
             <div class="posts" x-data>
-                <div class="open_model" @click="$dispatch('open-form')">
-                    <div class="open_ps">
-                        <img src="{{ auth()->user()->image() }}" alt="user">
-                        <input type="text" placeholder="What are you thinking about?" readonly>
-                    </div>
-                    <div class="cta_icons">
-                        <div class="icons">
-                            <i class='bx bx-camera'></i>
-                            <i class='bx bx-image-alt'></i>
-                            <i class='bx bx-happy'></i>
-                        </div>
-                        <button>Post</button>
-                    </div>
-                </div>
+                <x-posts.post-form-trigger />
                 <div class="modify_pubs">
                     <h2>Your Publications</h2>
                     <div class="mp_btns">
