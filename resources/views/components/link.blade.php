bee73ebbd2f7ccc7c22067f6f76560fd29369b06
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-11-15 18:29:54 +0100

    fixed profile active link on other users

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -6,11 +6,14 @@
                 ->route()
                 ->uri(),
         )[0] === 'settings' && $route === 'settings.account';
+    $route_array = explode('/', url()->current());
+    $is_auth = $route_array[count($route_array) - 1] === auth()->user()->slug;
     $is_profile = $route === 'profile';
     $slug = auth()->user()->slug;
 @endphp
 <a wire:navigate href="{{ $is_profile ? route($route, ['slug' => $slug]) : route($route) }}">
-    <li class="{{ request()->routeIs($route) || $is_settings ? 'active' : '' }}">
+    <li
+        class="{{ (request()->routeIs($route) && !$is_profile) || ($is_auth && $is_profile) || $is_settings ? 'active' : '' }}">
         <i class="fa-solid {{ $icon }}"></i>
         <span>{{ isset($name) ? $name : $route }}</span>
     </li>
