d7de1ce72efd8c658aba6c27ca518e30251984ca
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-12-01 18:49:55 +0100

    added app-settings livewire component

---
+++ app/Livewire/Settings/ApplicationSettings.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire\Settings;
+
+use Livewire\Component;
+
+class ApplicationSettings extends Component
+{
+
+
+    public function render()
+    {
+        return view('livewire.settings.application-settings');
+    }
+}
---
+++ resources/views/livewire/settings/application-settings.blade.php
@@ -0,0 +1,37 @@
+<div class="settings_content_application">
+    <div class="app_settings_card">
+        <div class="controls">
+            <label for="theme">Theme</label>
+            <div>
+                <p id="theme">Dark</p>
+                <div class="toggle-switch">
+                    <label class="switch-label">
+                        <input type="checkbox" class="checkbox">
+                        <span class="slider"></span>
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="controls">
+            <p>Language</p>
+            <select name="language" id="language">
+                <option value="Français">Français</option>
+                <option value="Français">Anglais</option>
+                <option value="Français">Arabe</option>
+            </select>
+        </div>
+        <div class="controls">
+            <label for="notifications">Notifications</label>
+            <div class="checkbox-wrapper-2">
+                <span>ON</span>
+                <input style="cursor: pointer;" type="checkbox" class="sc-gJwTLC ikxBAC">
+            </div>
+        </div>
+        <div class="btns controls">
+            <button><i class="fa-solid fa-triangle-exclamation"></i><span>Privacy Policy</span><i
+                    class="fa-solid fa-chevron-right"></i></button>
+            <button><i class="fa-solid fa-circle-question"></i><span>Support</span><i
+                    class="fa-solid fa-chevron-right"></i></button>
+        </div>
+    </div>
+</div>
