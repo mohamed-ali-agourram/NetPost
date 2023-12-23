961db5519e5aa3fcafe9c5ee0509933172247a44
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-12-23 15:50:02 +0100

    added explore_links on 425px screens

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -34,18 +34,21 @@
             <livewire:utilities.navbar />
             {{ $slot }}
         </main>
-        <div class="explore explore_links">
-            @php
-                $route = request()
-                    ->route()
-                    ->getName();
-            @endphp
-            <p>Explore the NET</p>
-            <a @class(['is_active' => $route === 'home']) wire:navigate href="{{ route('home') }}"><i
-                    class="fa-solid fa-globe"></i>Freinds</a>
-            <a @class(['is_active' => $route === 'freinds-posts']) wire:navigate href="{{ route('freinds-posts') }}"><i
-                    class="fa-solid fa-users"></i>Community</a>
-        </div>
+        @php
+            $route = request()
+                ->route()
+                ->getName();
+        @endphp
+        @if ($route === 'freinds-posts' || $route === 'home')
+            <div class="explore explore_links">
+                <p>Explore the NET</p>
+                <a @class(['is_active' => $route === 'home']) wire:navigate href="{{ route('home') }}"><i
+                        class="fa-solid fa-globe"></i>Freinds</a>
+                <a @class(['is_active' => $route === 'freinds-posts']) wire:navigate href="{{ route('freinds-posts') }}"><i
+                        class="fa-solid fa-users"></i>Community</a>
+            </div>
+        @endif
+
     </div>
 
     <script src="{{ asset('js/effects.js') }}"></script>
