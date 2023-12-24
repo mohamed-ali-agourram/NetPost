d2c977790369ddd687539981458016cd5045c691
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-12-24 12:30:19 +0100

    added glowAnimation on light mode

--- resources/views/components/posts/posts-list.blade.php
+++ resources/views/components/posts/posts-list.blade.php
@@ -7,10 +7,10 @@
     @empty
         <h2 style="color: gray; text-align: center; padding: 2vh;">No Post Found...</h2>
     @endforelse
-    <div style="margin-bottom: -15px" x-intersect="$wire.load_more()"></div>
-    @unless ($is_bottom && !$loadingMore)
-        <x-utilities.skeleton />
-    @endunless
+    {{-- <div style="margin-bottom: -15px" x-intersect="$wire.load_more()"></div> --}}
+    {{-- @unless ($is_bottom && !$loadingMore) --}}
+    <x-utilities.skeleton />
+    {{-- @endunless --}}
     <style>
         .skeleton_wrapper {
             .skeleton {
